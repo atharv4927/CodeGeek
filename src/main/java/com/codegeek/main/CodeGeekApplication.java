975caa8e0bc002a54546f6dec282120476bb5432
Atharv Mangesh Deshmukh <atharvdeshmukh4927@gmail.com>
2023-10-06 15:48:39 +0530

    Initial commit

---
+++ src/main/java/com/codegeek/main/CodeGeekApplication.java
@@ -0,0 +1,9 @@
+package com.codegeek.main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CodeGeekApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGeekApplication.class, args);
+	}
+}
